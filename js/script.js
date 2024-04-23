2088029369bc2a51ab6d2b360838884bf2e1a8a4
alejandro <alejandrozapata73@gmail.com>
2024-04-23 15:38:26 -0300

    subiendo js

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// alert("hola soy una ventana de Script externo")
+
+//tipos datos....Caractes
+// numeros- enteros -decimales    10  25   36, 32.5 1.5 etc
+//booleans---true o false / 1 0 / 
+
+console.log(10+5)
+console.log(3*2)
+console.log(10%2)
+console.log(10%3)
+console.log(10%7)
+
+// alert("10+5")
+// alert("100+1")
+
+
+//hola esto es un comentario
+alert("Pablo" + "Edad: " + (15+15))
+ /*
+esto es un bloque de codigo
+probando nuevas cosas
+ */
+
+// alert("Beatriz Noemi")
+
+// console.log("beatriz NoeMi")
+
+// document.write("Beatriz Noemi")
+
+//variables...una caja con un nombre
+
+ // cadena, un caracter, entero, decimal, booleano
+ 
+ /*Pseudocodigo
+
+ 1-definir la variable nombre
+ 2-asignar un valor del tipo cadena de carater "claudia"
+ 3
+
+
+ */
+
+ var nombre = "claudia"
+
+ console.log(nombre)
+ alert("El nombre de usuario es: " + nombre)
+ document.write(nombre)
+
+//realizar el algoritmo sacar superficie de un cuadrado
+
+
+//lado * lado
+//lado (2)
+
+var ladoA = 10 //
+var ladoB = 10 //
+
+var superCuadrado = ladoA * ladoB
+
+console.log("La superficie del cuadrado es: " + superCuadrado)
+alert("")
+document.write("La superficie del cuadrado es: " + superCuadrado)
+
+//
+
